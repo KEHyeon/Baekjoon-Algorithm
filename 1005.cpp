19c55867a15cd7e3541931a91cbd066152ece252
KEHyeon <kth050213@naver.com>
2022-05-20 14:08:59 +0900

    2202-05-20

---
+++ 1005.cpp
@@ -0,0 +1,41 @@
+##include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	int tc; cin >> tc;
+	while(tc--) {
+		int n, k; cin >> n >> k;
+		vector<int> graph[n + 1];
+		int time[n + 1];
+		bool isUsed[n + 1] = {0,};
+		int indegree[n + 1] = {0,};
+		int dp[n + 1] = {0,};
+		queue<int> q;
+		for(int i = 1; i <= n; i++) cin >> time[i];
+		while(k--) {
+			int a, b; cin >> a >> b;
+			graph[a].push_back(b);
+			indegree[b]++;
+		}
+		for(int i = 1; i <= n; i++) {
+			if(indegree[i] == 0) q.push(i);
+		}
+		while(!q.empty()) {
+			int cur = q.front(); q.pop();
+			dp[cur] += time[cur];
+			for(auto nxt : graph[cur]) {
+				if(isUsed[nxt]) {
+					dp[nxt] = max(dp[nxt], dp[cur]);
+				}
+				else {
+					dp[nxt] = dp[cur];
+					isUsed[nxt] = 1;
+				}
+				indegree[nxt]--;
+				if(indegree[nxt] == 0) q.push(nxt);
+			}
+		}
+		int w; cin >> w;
+		cout << dp[w] << "\n";
+	}
+}
