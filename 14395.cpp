4438974385cda1a2bbd7100ca560a804ca5d655e
KEHyeon <kth050213@naver.com>
2022-09-05 11:27:26 +0900

    stack

--- 14395.cpp
+++ 14395.cpp
@@ -1,30 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
-int s, t;
-bool vis[100000001];
+typedef long long ll;
+set<ll> vis;
+ll s, t;
 int main() {
 	cin >> s >> t;
 	if(s == t) {
 		cout << 0;
 		return 0;
 	}
-	queue<pair<int,string>> q;
-	q.push({s, ""});
-	q.push({1, "/"});
+	queue<pair<ll,string>> q;
+	if(s > t) {
+		q.push({1,"/"});
+	}
+	else q.push({s, ""});
 	while(!q.empty()) {
 		auto cur = q.front(); q.pop();
-		int num = cur.first;
+		ll num = cur.first;
 		string s = cur.second;
+		if(num > t) continue;
 		if(num == t) {
 			cout << s;
 			return 0;
 		}
-		if(!vis[num * num] && num * num <= t) {
-			vis[num * num] = 1;
+		if(vis.find(num * num) == vis.end()) {
+			vis.insert(num * num);
 			q.push({num*num, s + '*'});
 		}
-		if(!vis[num + num] && num + num <= t) {
-			vis[num + num] = 1;
+		if(vis.find(num + num) == vis.end()) {
+			vis.insert(num + num);
 			q.push({num+num, s + '+'});
 		}
 	}
