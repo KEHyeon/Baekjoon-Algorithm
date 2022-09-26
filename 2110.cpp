a0aa21a9e38477392394a275feb01730f1b5fc82
KEHyeon <kth050213@naver.com>
2022-09-26 12:14:12 +0900

    psssss

---
+++ 2110.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, c, arr[200000];
+bool check(int x) {
+	int cnt = 1;
+	int now = arr[0];
+	for(int i = 1;  i < n; i++) {
+		if(arr[i] - now >= x) {
+			cnt++;
+			now = arr[i];
+		}
+	}
+	return cnt >= c;
+}
+int main() {
+	cin >> n >> c;
+	int lo = 1;
+	int hi = -1;
+	for(int i = 0; i < n; i++) {
+		cin >> arr[i];
+		hi = max(hi, arr[i]);
+	}
+	sort(arr, arr + n);
+	while(lo + 1 < hi) {
+		int mid = (lo + hi) / 2;
+		if(check(mid)) {
+			lo = mid;
+		}
+		else hi = mid;
+	}
+	cout << lo;
+}
