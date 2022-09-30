ceddc4455e15997136f5de61711ba2226ffc263b
KEHyeon <kth050213@naver.com>
2022-09-30 21:15:28 +0900

    ioio

---
+++ 3003.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	int n;
+	cin >> n;
+	cout << 1 - n << " ";
+	cin >> n;
+	cout << 1 - n << " ";
+	cin >> n;
+	cout << 2 - n << " ";
+	cin >> n;
+	cout << 2 - n << " ";
+	cin >> n;
+	cout << 2 - n << " ";
+	cin >> n;
+	cout << 8 - n << " ";
+}
