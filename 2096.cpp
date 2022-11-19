c51c245cb6bb85118d1d19d6f2c4af22c9ce71d1
KEHhyeon <kth050213@naver.com>
2022-11-19 21:23:42 +0900

    슬라이딩 윈도우 ㅋㅇㅋㅇㅎㅇㅋㅎㅋ

--- 2096.cpp
+++ 2096.cpp
@@ -1,3 +1,32 @@
-//
-// Created by kimtaehyun on 2022/11/17.
-//
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+int arr[100000][3], pre[3], cur[3];
+int dy[3] = {0, -1, 1};
+int solve(bool isMax) {
+    pre[0] = arr[0][0];
+    pre[1] = arr[0][1];
+    pre[2] = arr[0][2];
+    for(int i = 1; i < n; i++) {
+        for(int j = 0; j < 3; j++) {
+            cur[j] = isMax ? 0 : 1000000000;
+            for(int k = 0; k < 3; k++) {
+                int ny = j + dy[k];
+                if(ny < 0 || ny > 2) continue;
+                cur[j] = isMax ? max(cur[j], pre[ny] + arr[i][j]) : min(cur[j], pre[ny] + arr[i][j]);
+            }
+        }
+        pre[0] = cur[0];
+        pre[1] = cur[1];
+        pre[2] = cur[2];
+    }
+    return isMax ? *(max_element(pre, pre + 3)) : *(min_element(pre, pre + 3));
+}
+int main() {
+    cin >> n;
+    for(int i = 0; i < n; i++)
+        cin >> arr[i][0] >> arr[i][1] >> arr[i][2];
+
+    cout << solve(1) << " " << solve(0);
+
+}
--- 2225.cpp
+++ 2225.cpp
@@ -2,13 +2,21 @@
 using namespace std;
 int n, m;
 int memo[201][201];
-int f(int n, int k) {
-	if(n == 1 || k == 1) return 1;
-	if(memo[n][k]) return memo[n][k];
-	if(n <= k) return memo[n][k] = f(n, k-1) + n;
-	return memo[n][k] = f(n - 1, k) + f(n, k - 1);
+const int mod = 1000000000;
+int f(int k, int sum) {
+    if(k == 0 || sum < 0) {
+        return sum == 0;
+    }
+    int &ret = memo[k][sum];
+    if(ret != -1) return ret;
+    ret = 0;
+    for(int i = 0; i <= n; i++) {
+        ret = (ret + f(k - 1, sum - i)) % mod;
+    }
+    return ret;
 }
 int main() {
 	cin >> n >> m;
-	cout << f(n, m);
+    memset(memo, -1, sizeof(memo));
+	cout << f(m, n);
 }
